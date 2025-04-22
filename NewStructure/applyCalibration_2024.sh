cbbbb8fc7a5732e942187b5bb6ed4759367a7263
Friederike Bock <friederike.bock@cern.ch>
2025-04-22 09:28:16 -0400

    small modifications to run-scripts

--- NewStructure/applyCalibration_2024.sh
+++ NewStructure/applyCalibration_2024.sh
@@ -20,22 +20,29 @@ function Calib()
 	fi
 }
 
-dataDirRaw=""
-dataDirOut=""
 dataDirCal=""
 if [ $1 = "fbock" ]; then 
 	dataDirCal=/home/fbock/EIC/Analysis/LFHCalTB2024/CAENdata
-	dataDirRaw=/media/fbock/Samsung_T5/LFHCAL_TB/202408_PST09/CAENdata/MuonRuns
-	dataDirRawE=/media/fbock/Samsung_T5/LFHCAL_TB/202408_PST09/CAENdata/ElectronRuns
-	dataDirRawH=/media/fbock/Samsung_T5/LFHCAL_TB/202408_PST09/CAENdata/HadronRuns
+	dataDirIn=/media/fbock/Samsung_T5/LFHCAL_TB/202408_PST09/CAENdata/MuonRuns
+	dataDirInE=/media/fbock/Samsung_T5/LFHCAL_TB/202408_PST09/CAENdata/ElectronRuns
+	dataDirInH=/media/fbock/Samsung_T5/LFHCAL_TB/202408_PST09/CAENdata/HadronRuns
 	dataDirOut=/home/fbock/EIC/Analysis/LFHCalTB2024/CAENdata/MuonRuns
 	dataDirOutE=/home/fbock/EIC/Analysis/LFHCalTB2024/CAENdata/ElectronRuns
 	dataDirOutH=/home/fbock/EIC/Analysis/LFHCalTB2024/CAENdata/HadronRuns
+elif [ $1 = "fbockExt" ]; then 
+	dataDirCal=/home/fbock/EIC/Analysis/LFHCalTB2024/CAENdata
+	dataDirBase=/media/fbock/Samsung_T5/LFHCAL_TB/202408_PST09/CAENdata
+	dataDirIn=$dataDirBase/MuonRuns
+	dataDirInE=$dataDirBase/ElectronRuns
+	dataDirInH=$dataDirBase/HadronRuns
+	dataDirOut=$dataDirIn
+	dataDirOutE=$dataDirInE
+	dataDirOutH=$dataDirInH
 elif [ $1 = "eglimos" ]; then 
 	dataDirCal=/home/ewa/EIC/test_beam2024/fullScanC/Output/MuonRuns
-	dataDirRaw=/home/ewa/EIC/test_beam2024/fullScanC
-	dataDirRawE=/home/ewa/EIC/test_beam2024/fullScanC
-	dataDirRawH=/home/ewa/EIC/test_beam2024/fullScanC
+	dataDirIn=/home/ewa/EIC/test_beam2024/fullScanC
+	dataDirInE=/home/ewa/EIC/test_beam2024/fullScanC
+	dataDirInH=/home/ewa/EIC/test_beam2024/fullScanC
 	dataDirOut=/home/ewa/EIC/test_beam2024/fullScanC/Output/MuonRuns
 	dataDirOutE=/home/ewa/EIC/test_beam2024/fullScanC/Output/ElectronRuns
 	dataDirOutH=/home/ewa/EIC/test_beam2024/fullScanC/Output/HadronRuns
@@ -53,23 +60,22 @@ if [ $2 == "ScanA" ]; then
 	#muon runs
 	muonScanA_45V='244 250 282 283'
 	pedScanA_45V='271 277'
-	Calib $3 $calibFile1 $dataDirRaw $dataDirOut muonScanA1_45V ../PlotsMuonCalibrated_2024/Run_
-	Calib $3 $calibFile2 $dataDirRaw $dataDirOut muonScanA2_45V ../PlotsMuonCalibrated_2024/Run_
+# 	Calib $3 $calibFile1 $dataDirIn $dataDirOut muonScanA1_45V ../PlotsMuonCalibrated_2024/Run_
+# 	Calib $3 $calibFile2 $dataDirIn $dataDirOut muonScanA2_45V ../PlotsMuonCalibrated_2024/Run_
 	
-	#electron runs
+# 	electron runs
 	runs='251 252 254 257 258 ' 
+# 	runs=' 251 ' 
 	for runNr in $runs; do 
-		Calib $3 $calibFile1 $dataDirRawE $dataDirOutE $runNr ../PlotsElectronCalibrated_2024/Run_
+		Calib $3 $calibFile1 $dataDirInE $dataDirOutE $runNr ../PlotsElectronCalibrated_2024/Run_
 	done;
 	
-	#hadron runs
-	runs='261 264 265 269 270 272 274 275 ' 
-	for runNr in $runs; do 
-		Calib $3 $calibFile1 $dataDirRawH $dataDirOutH $runNr ../PlotsHadronCalibrated_2024/Run_
-	done;
-fi
-
-if [ $2 == "ScanB" ]; then
+# 	#hadron runs
+# 	runs='261 264 265 269 270 272 274 275 ' 
+# 	for runNr in $runs; do 
+# 		Calib $3 $calibFile1 $dataDirInH $dataDirOutH $runNr ../PlotsHadronCalibrated_2024/Run_
+# 	done;
+elif [ $2 == "ScanB" ]; then
 	echo "running calibrate for 42V runs, campaign B"
 	calibFile1=$dataDirCal/CalibFinal_muonScanB1_42V.root 
 	calibFile2=$dataDirCal/CalibFinal_muonScanB2_42V.root
@@ -79,48 +85,79 @@ if [ $2 == "ScanB" ]; then
 	pedScanB1_42V='332'
 	muonScanB2_42V='370 371 374'
 	pedScanB2_42V='369'
-# 	Calib $3 $calibFile1 $dataDirRaw $dataDirOut muonScanB1_42V ../PlotsMuonCalibrated_2024/Run_
-# 	Calib $3 $calibFile2 $dataDirRaw $dataDirOut muonScanB2_42V ../PlotsMuonCalibrated_2024/Run_
+# 	Calib $3 $calibFile1 $dataDirIn $dataDirOut muonScanB1_42V ../PlotsMuonCalibrated_2024/Run_
+	Calib $3 $calibFile2 $dataDirIn $dataDirOut muonScanB2_42V ../PlotsMuonCalibrated_2024/Run_
 	
 # 	#electron runs
 # 	runs='333 334 336 337 338 ' 
-	runs='338 ' 
-	for runNr in $runs; do 
-		Calib $3 $calibFile1 $dataDirRawE $dataDirOutE $runNr ../PlotsElectronCalibrated_2024/Run_
-	done;
+# 	runs='338 ' 
+# 	for runNr in $runs; do 
+# 		Calib $3 $calibFile1 $dataDirInE $dataDirOutE $runNr ../PlotsElectronCalibrated_2024/Run_
+# 	done;
 # 	
 # 	#hadron runs
 # 	runs='340 349 346 350 357 360 362 367 368' 
 # 	for runNr in $runs; do 
-# 		Calib $3 $calibFile1 $dataDirRawH $dataDirOutH $runNr ../PlotsHadronCalibrated_2024/Run_
+# 		Calib $3 $calibFile1 $dataDirInH $dataDirOutH $runNr ../PlotsHadronCalibrated_2024/Run_
 # 	done;
-fi
 
-if [ $2 == "ScanC" ]; then
+elif [ $2 == "ScanC" ]; then
 	echo "running calibrate for 43.5V runs, campaign C"
-	calibFile1=$dataDirCal/rawPedAndMuonWBCImp_muonScanC1_43_5V.root 
-	calibFile2=$dataDirCal/rawPedAndMuonWBCImp_muonScanC2_43_5V.root
-
+	if [ $1 == "fbock" ] || [ $1 == "fbockExt" ]; then 
+		calibFile1=$dataDirCal/CalibFinal_muonScanC1_43_5V.root
+		calibFile2=$dataDirCal/CalibFinal_muonScanC2_43_5V.root
+	else 
+		calibFile1=$dataDirCal/rawPedAndMuonWBCImp_muonScanC1_43_5V.root 
+		calibFile2=$dataDirCal/rawPedAndMuonWBCImp_muonScanC2_43_5V.root
+	fi
 	#muon runs
-	muonScanC1_43V='376 375'
-	pedScanC1_43V='377'
-	muonScanC2_43V='405 410 408'
-	pedScanC2_43V='404'
-	# Calib $3 $calibFile1 $dataDirRaw $dataDirOut muonScanC1_43_5V ../PlotsMuonCalibrated_2024/Run_376
-	# Calib $3 $calibFile2 $dataDirRaw $dataDirOut muonScanB2_42V ../PlotsMuonCalibrated_2024/Run_
-	
-# 	#electron runs
-	# runs='379 380 381 384 387' 
-	runs='380' 
-	for runNr in $runs; do 
-		Calib $3 $calibFile1 $dataDirRawE $dataDirOutE $runNr ../PlotsElectronCalibrated_2024/Run_
-	done;
+# 	muonScanC1_43V='376 375'
+# 	pedScanC1_43V='377'
+# 	muonScanC2_43V='405 410 408'
+# 	pedScanC2_43V='404'
+	Calib $3 $calibFile1 $dataDirIn $dataDirOut muonScanC1_43_5V ../PlotsMuonCalibrated_2024/Run_
+	Calib $3 $calibFile2 $dataDirIn $dataDirOut muonScanC2_43_5V ../PlotsMuonCalibrated_2024/Run_
 # 	
+# # 	#electron runs
+# # 	runs='379 380 381 384 387' 
+# # 	runs='380' 
+# 	for runNr in $runs; do 
+# 		Calib $3 $calibFile1 $dataDirInE $dataDirOutE $runNr ../PlotsElectronCalibrated_2024/Run_
+# 	done;
+# 	
+# 	hadron runs
+# 	runs='397 398 399 401' 
+	runs='390 392 393 394 397 398 399 401' 
+# 	runs='397 398 399 401' 
+# 	runs='398 399 401' 
+	for runNr in $runs; do 
+		Calib $3 $calibFile1 $dataDirInH $dataDirOutH $runNr ../PlotsHadronCalibrated_2024/Run_
+	done;
+elif [ $2 == "ScanD" ]; then
+# 	calibFile1=$dataDirCal/CalibFinal_muonScanB1_42V.root 
+	calibFile2=$dataDirCal/CalibFinal_muonScanD2_45V.root
+	echo "running calibrate for 45V runs, campaing A1"
+	
+	#muon runs
+	muonScanD1_45V='412 417'		# these runs are messed up don't analyze
+	pedScanD1_45V='420'
+	# 29.3K events
+	muonScanD2_45V='460 456 457'
+	pedScanD2_45V='454'
+	Calib $3 $calibFile2 $dataDirIn $dataDirOut muonScanD2_45V ../PlotsMuonCalibrated_2024/Run_
+	
+# 	electron runs
+	runs='421 422 429 430 432 ' 
+	for runNr in $runs; do 
+		Calib $3 $calibFile2 $dataDirInE $dataDirOutE $runNr ../PlotsElectronCalibrated_2024/Run_
+	done;
+	
 	#hadron runs
-	# runs='397 398 399 401' 
-	# runs='397' 
-	# for runNr in $runs; do 
-	# 	Calib $3 $calibFile1 $dataDirRawH $dataDirOutH $runNr ../PlotsHadronCalibrated_2024/Run_
-	# done;
+	runs='434 437 439 441 444 445 449 452 ' 
+# 	runs='437 439 441 444 445 449 452 ' 
+# 	runs='441 444 445 449 452 ' 
+	for runNr in $runs; do 
+		Calib $3 $calibFile2 $dataDirInH $dataDirOutH $runNr ../PlotsHadronCalibrated_2024/Run_
+	done;
 fi
 
